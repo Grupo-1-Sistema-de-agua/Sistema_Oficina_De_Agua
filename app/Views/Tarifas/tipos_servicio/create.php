b185a43a521c311af034661d3b3cb1ee46f7e72e
mijelilucero <mlucerob@miumg.edu.gt>
2026-09-11 01:24:09 -0600

    SDGA-71 fix(auth): usar csrf nativo en crear tipo de servicio

--- app/Views/Tarifas/tipos_servicio/create.php
+++ app/Views/Tarifas/tipos_servicio/create.php
@@ -15,7 +15,7 @@
   <?php endif; ?>
 
   <form action="<?= base_url('tipos-servicio') ?>" method="post" class="col-lg-6">
-    <?= csrf_field() ?>
+    <?= csrf_field_nativo() ?>
 
     <div class="form-outline mb-4" data-mdb-input-init>
       <input type="text" class="form-control" id="nombre" name="nombre"
