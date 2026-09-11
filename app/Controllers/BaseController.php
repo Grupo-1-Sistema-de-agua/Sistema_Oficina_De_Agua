5b288c4545f383df10eac6c840ab874412108dc6
mijelilucero <mlucerob@miumg.edu.gt>
2026-09-11 00:56:39 -0600

    SDGA-71 fix(auth): cargar helper de CSRF en BaseController

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -24,7 +24,7 @@ abstract class BaseController extends Controller
 {
 
     protected $session;
-    protected $helpers = ['flash'];
+    protected $helpers = ['flash', 'csrf'];
 
     /**
      * @return void
