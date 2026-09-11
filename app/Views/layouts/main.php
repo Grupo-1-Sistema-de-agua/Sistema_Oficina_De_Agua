f00053d9762a0d3760a144d356fccdd6b40e7977
mijelilucero <mlucerob@miumg.edu.gt>
2026-09-11 03:54:08 -0600

    SDGA-71 fix(auth): quitar enlace de cambiar contrasena del menu, ahora vive en usuarios

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -43,9 +43,6 @@
             <a href="<?= base_url('admin/usuarios') ?>" class="list-group-item list-group-item-action py-2">
               <i class="fas fa-user-shield fa-fw me-3"></i><span>Usuarios</span>
             </a>
-            <a href="<?= base_url('admin/password') ?>" class="list-group-item list-group-item-action py-2">
-              <i class="fas fa-key fa-fw me-3"></i><span>Cambiar contrasena</span>
-            </a>
           <?php endif; ?>
         </div>
       </div>
